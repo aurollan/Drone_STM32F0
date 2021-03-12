8de0f8754824d2a239908ce398e229b79ef6aedb
aurollan <aurollan@debian.home>
2021-03-12 03:08:02 -0500

    [REFAC] i2c communication function

--- 13.I2C_TX_RX/src/dronef0_i2c_rd_wr.c
+++ 13.I2C_TX_RX/src/dronef0_i2c_rd_wr.c
@@ -31,26 +31,72 @@ uint32_t i2c_wait_flag_status(I2C_TypeDef* I2Cx , uint32_t i2c_flag, FlagStatus 
  */
 uint32_t i2c_error_occur(I2C_TypeDef* I2Cx)
 {
-    uint32_t i2c_isr_err;
+    uint32_t    i2c_isr_err_flags;
 
-    i2c_isr_err = 0;
+    i2c_isr_err_flags = 0;
     if (SET == I2C_GetFlagStatus(I2Cx, I2C_FLAG_BERR))
     {
-        i2c_isr_err |= I2C_FLAG_BERR;
+        i2c_isr_err_flags |= I2C_FLAG_BERR;
     }
     if (SET == I2C_GetFlagStatus(I2Cx, I2C_FLAG_BUSY))
     {
-        i2c_isr_err |= I2C_FLAG_BUSY;
+        i2c_isr_err_flags |= I2C_FLAG_BUSY;
     }
     if (SET == I2C_GetFlagStatus(I2Cx, I2C_FLAG_ARLO))
     {
-        i2c_isr_err |= I2C_FLAG_ARLO;
+        i2c_isr_err_flags |= I2C_FLAG_ARLO;
     }
     if (SET == I2C_GetFlagStatus(I2Cx, I2C_FLAG_OVR))
     {
-        i2c_isr_err |= I2C_FLAG_OVR;
+        i2c_isr_err_flags |= I2C_FLAG_OVR;
     }
-    return (i2c_isr_err);
+    return (i2c_isr_err_flags);
+}
+
+/**
+ * @brief   receive data from device and check if everything went right
+ * @param   I2Cx   I2C_TypeDef*.   I2C channel selected
+ * @return  i2c_isr_err_occured uint32_t. Contains only I2C ISR register error bits
+ */
+uint32_t I2C_receive_data_and_wait(I2C_TypeDef* I2Cx, uint8_t* received_data)
+{
+    uint32_t    i2c_isr_err_occured;
+
+    i2c_isr_err_occured = 0;
+    i2c_isr_err_occured |= i2c_wait_flag_status(I2Cx, I2C_FLAG_RXNE, SET, I2C_MAX_RETRY);
+    if (!i2c_isr_err_occured)
+    {
+        *received_data = I2C_ReceiveData(I2Cx);
+    }
+    i2c_isr_err_occured |= i2c_error_occur(I2Cx);
+    if (!i2c_isr_err_occured) 
+    {
+        i2c_isr_err_occured = i2c_wait_flag_status(I2Cx, I2C_FLAG_TC, SET, I2C_MAX_RETRY);
+    }
+    return (i2c_isr_err_occured);
+}
+
+/**
+ * @brief   send data to device and check if everything went right
+ * @param   I2Cx    I2C_TypeDef*.   I2C channel selected
+ * @return  i2c_isr_err_occured uint32_t. Contains only I2C ISR register error bits
+ */
+uint32_t I2C_send_data_and_wait(I2C_TypeDef* I2Cx, uint8_t data_to_send)
+{
+    uint32_t    i2c_isr_err_occured;
+
+    i2c_isr_err_occured = 0;
+    i2c_isr_err_occured = i2c_wait_flag_status(I2Cx, I2C_FLAG_TXE, SET, I2C_MAX_RETRY);
+    if (!i2c_isr_err_occured)
+    {
+        I2C_SendData(I2Cx, data_to_send);
+    }
+    i2c_isr_err_occured |= i2c_error_occur(I2Cx);
+    if (!i2c_isr_err_occured) 
+    {
+        i2c_isr_err_occured = i2c_wait_flag_status(I2Cx, I2C_FLAG_TC, SET, I2C_MAX_RETRY);
+    }
+    return (i2c_isr_err_occured);
 }
 
 /**
@@ -59,34 +105,35 @@ uint32_t i2c_error_occur(I2C_TypeDef* I2Cx)
  * @param   register_to_read            uint8_t.        Peripheral register to start to read from
  * @param   received_data               uint8_t*.       Memory to write data to
  * @param   number_of_bytes_to_read     uint8_t.        Number of bytes to read from peripheral
- * @return  i2c_isr_err uint32_t. Contains only I2C ISR register error bits
+ * @return  i2c_isr_err_occured uint32_t. Contains only I2C ISR register error bits
  */
 uint32_t i2c_read(I2C_TypeDef* I2Cx, uint16_t register_to_read, uint8_t *received_data, uint8_t number_of_bytes_to_read)
 {
     uint32_t    index;
-    uint32_t    i2c_isr_err;
+    uint32_t    i2c_isr_err_occured;
 
-    index                   = 0;
-    i2c_isr_err             = 0;;
+    index       = 0;
+    i2c_isr_err_occured = 0;
     I2C_TransferHandling(I2Cx, register_to_read, 1, I2C_Generate_Start_Read, I2C_SoftEnd_Mode);
-    I2C_SendData(I2Cx, register_to_read);
-    i2c_isr_err |= i2c_wait_flag_status(I2Cx, I2C_FLAG_TC, SET, I2C_MAX_RETRY);
-    i2c_isr_err |= i2c_error_occur(I2Cx);
-    if (!i2c_isr_err)
+    i2c_isr_err_occured  = I2C_send_data_and_wait(I2Cx, register_to_read);
+    if (!i2c_isr_err_occured)
     {
-        i2c_isr_err |= i2c_wait_flag_status(I2Cx, I2C_FLAG_RXNE, SET, I2C_MAX_RETRY);
-        if (!i2c_isr_err)
+        I2C_TransferHandling(I2Cx, register_to_read, number_of_bytes_to_read, I2C_Generate_Start_Read, I2C_AutoEnd_Mode);
+        while (index < number_of_bytes_to_read)
         {
-            I2C_TransferHandling(I2Cx, register_to_read, number_of_bytes_to_read, I2C_Generate_Start_Read, I2C_AutoEnd_Mode);
-            while (index < number_of_bytes_to_read) {
-                received_data[index] = I2C_ReceiveData(I2Cx);
-                index++;
+            i2c_isr_err_occured = I2C_receive_data_and_wait(I2Cx, &(received_data[index]));
+            if (i2c_isr_err_occured)
+            {
+                break;
             }
-            i2c_isr_err = i2c_wait_flag_status(I2Cx, I2C_FLAG_STOPF, SET, I2C_MAX_RETRY);
-            i2c_isr_err |= i2c_error_occur(I2Cx);
+            index++;
+        }
+        if (!i2c_isr_err_occured)
+        {
+            i2c_isr_err_occured = i2c_wait_flag_status(I2Cx, I2C_FLAG_STOPF, SET, I2C_MAX_RETRY);
         }
     }
-    return (i2c_isr_err);
+    return (i2c_isr_err_occured);
 }
 
 /**
@@ -94,32 +141,25 @@ uint32_t i2c_read(I2C_TypeDef* I2Cx, uint16_t register_to_read, uint8_t *receive
  * @param   I2Cx              I2C_TypeDef*. I2C channel selected
  * @param   register_to_write uint8_t.      Peripheral register to write
  * @param   data_to_write     uint8_t.      Data to write to register
- * @return  i2c_isr_err       uint32_t.     Contains only register error bits
+ * @return  i2c_isr_err_occured       uint32_t.     Contains only register error bits
  */
 uint32_t i2c_write(I2C_TypeDef* I2Cx, uint8_t register_to_write, uint8_t data_to_write)
 {
-    uint32_t    i2c_isr_err;
+    uint32_t    i2c_isr_err_occured;
     uint32_t    number_of_bytes_to_read;
     uint32_t    number_of_bytes_to_write;
 
-    i2c_isr_err              = 0;
+    i2c_isr_err_occured              = 0;
     number_of_bytes_to_read  = 1;
     number_of_bytes_to_write = 1;
     I2C_TransferHandling(I2Cx, register_to_write, number_of_bytes_to_read, I2C_Generate_Start_Read, I2C_SoftEnd_Mode);
-    I2C_SendData(I2Cx, register_to_write);
-    i2c_isr_err |= i2c_wait_flag_status(I2Cx, I2C_FLAG_TC, SET, I2C_MAX_RETRY);
-    i2c_isr_err |= i2c_error_occur(I2Cx);
-    if (!i2c_isr_err)
+    i2c_isr_err_occured = I2C_send_data_and_wait(I2Cx, register_to_write);
+    if (!i2c_isr_err_occured)
     {
-        i2c_isr_err |= i2c_wait_flag_status(I2Cx, I2C_FLAG_RXNE, SET, I2C_MAX_RETRY);
-        if (0 == i2c_isr_err)
-        {
             I2C_TransferHandling(I2Cx, register_to_write, number_of_bytes_to_write, I2C_Generate_Start_Write, I2C_AutoEnd_Mode);
-            I2C_SendData(I2Cx, data_to_write);
-            i2c_isr_err |= i2c_wait_flag_status(I2Cx, I2C_FLAG_STOPF, SET, I2C_MAX_RETRY);
-            i2c_isr_err |= i2c_error_occur(I2Cx);
-        }
+            i2c_isr_err_occured = I2C_send_data_and_wait(I2Cx, data_to_write);
+            i2c_isr_err_occured |= i2c_wait_flag_status(I2Cx, I2C_FLAG_STOPF, SET, I2C_MAX_RETRY);
     }
-    return (i2c_isr_err);
+    return (i2c_isr_err_occured);
 }
 
